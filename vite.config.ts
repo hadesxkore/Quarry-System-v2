21d25a8bfa776a5a6d0faef8b394eff671b0b243
Your Name <your.email@example.com>
2026-04-21 08:25:49 +0800

    12332

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/firestore\.googleapis\.com\/.*/i,
